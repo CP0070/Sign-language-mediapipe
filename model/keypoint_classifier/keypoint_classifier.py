ba33e9c7c2e4954e9152698b5ee37a04750ce09a
CP0070 <tripathi.cp07@gmail.com>
2024-04-19 18:17:37 +0530

    DONE

--- model/keypoint_classifier/keypoint_classifier.py
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 class KeyPointClassifier(object):
     def __init__(
         self,
-        model_path='model/keypoint_classifier/converted_model.tflite',
+        model_path='model/keypoint_classifier/converted_model1.tflite',
         num_threads=1,
     ):
         self.interpreter = tf.lite.Interpreter(model_path=model_path,
