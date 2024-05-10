98a4e367cfcdf792a044c5be59104512c22e3bc5
CP0070 <tripathi.cp07@gmail.com>
2024-05-10 23:36:02 +0530

    DONE

--- webpage.py
+++ webpage.py
@@ -12,5 +12,14 @@ def main():
         if error:
             st.error(error.decode("utf-8"))
 
+
+    if st.button("Gloves"):
+        st.write("starting server")
+        process = subprocess.Popen(["python", "model/sensor_classifier/Sensor_data_client.py"], stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        st.write(output.decode("utf-8"))
+        if error:
+            st.error(error.decode("utf-8"))
+
 if __name__ == "__main__":
     main()
